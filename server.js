620256bafc2829ce1aae3665c0c4ef75b74f5101
Scott Smereka <scott@livioradio.com>
2013-07-02 11:17:11 -0400

    Updated server connection to database to wait for a connection before continuing to avoid TTL errors.

--- server.js
+++ server.js
@@ -1,44 +1,48 @@
-var express = require('express'),                                    // We will use express to handle routes and setup of our server.
+var express = require('express'),                                      // We will use express to handle routes and setup of our server.
     expressValidator = require('express-validator'),  
-    configModule = require('./config/config'),                       // This is a module that will handle basic server configuraitons.
-    config = configModule.config(),                                  // This is our configuration object that stores global settings for the server..
-    port = (config.port),                                            // You can set the port using "export PORT=1234" or it will default to your configuration file.
-    passport = require('passport'),                                  // Handles authentication
-    flash = require('connect-flash'),                                // Enables flash messages while authenticating with passport.
-    mongoose = require('mongoose'),                                  // Object modeling for mongodb database.
-    MongoStore = require('connect-mongo')(express),                  // Session store for MongoDB connection.
-    fs = require('fs'),                                              // Allows use of the filesystem.
-    app = module.exports = express();                                // Get a new express server.
+    configModule = require('./config/config'),                         // This is a module that will handle basic server configuraitons.
+    config = configModule.config(),                                    // This is our configuration object that stores global settings for the server..
+    port = (config.port),                                              // You can set the port using "export PORT=1234" or it will default to your configuration file.
+    passport = require('passport'),                                    // Handles authentication
+    flash = require('connect-flash'),                                  // Enables flash messages while authenticating with passport.
+    mongoose = require('mongoose'),                                    // Object modeling for mongodb database.
+    MongoStore = require('connect-mongo')(express),                    // Session store for MongoDB connection.
+    fs = require('fs'),                                                // Allows use of the filesystem.
+    app = module.exports = express();                                  // Get a new express server.
 
-configModule.configureEnviorment(express, app, config);              // Handle different operating modes set by using "export NODE_ENV=local" or "export NODE_ENV=development" or etc.
-app.use(express.cookieParser());                                     // Setup express: enable cookies.
-app.use(express.bodyParser());                                       // Setup express: enable body parsing.
+configModule.configureEnviorment(express, app, config);                // Handle different operating modes set by using "export NODE_ENV=local" or "export NODE_ENV=development" or etc.
+app.use(express.cookieParser());                                       // Setup express: enable cookies.
+app.use(express.bodyParser());                                         // Setup express: enable body parsing.
 app.use(expressValidator);
-app.use(flash());                                                    // Setup connect-flash enabling flash messages for passport.
-var db = setupDatabase(app, config);                                 // Setup the database and secret sessions.  This must be run after express cookie parser is setup.
-app.use(passport.initialize());                                      // Setup passport for authentication.
-app.use(passport.session());                                         // Setup passport sessions.  Make sure this is called after setting up express sessions.
+app.use(flash());                                                      // Setup connect-flash enabling flash messages for passport.
+setupDatabase(app, config, function(err, db) {                         // Setup the database and secret sessions.  This must be run after express cookie parser is setup.  This call will block until the database is connected.
+  if(err || ! db) 
+    return console.log("[ ERROR ] Could setup and connect to the database. \n" + err);
 
-app.use(express.static(config.dirname + config.paths.publicFolder)); // Set the public folder as a static route. The public folder stores css, images, js, etc.
-app.use(app.router);                                                 // Handle all routes
-app.use(express.favicon(config.dirname + config.paths.favIcon));     // Display a favicon, use  express.favicon() to use the express default favicon.
+  app.use(passport.initialize());                                      // Setup passport for authentication.
+  app.use(passport.session());                                         // Setup passport sessions.  Make sure this is called after setting up express sessions.
 
-app.set('views', config.dirname + config.paths.viewsFolder);         // Set up the root directory for our views.
-app.set('view engine', 'jade');                                      // Set our view engin as JADE.
+  app.use(express.static(config.dirname + config.paths.publicFolder)); // Set the public folder as a static route. The public folder stores css, images, js, etc.
+  app.use(app.router);                                                 // Handle all routes
+  app.use(express.favicon(config.dirname + config.paths.favIcon));     // Display a favicon, use  express.favicon() to use the express default favicon.
 
-// Load our routes here, in the order they will be called.
-loadFolder(config.paths.middlewareFolder, app, db, config);          // Load the middleware controllers.
-app.use(allowCors);                                                  // Allow cross origin requests
-loadFolder(config.paths.controllersFolder, app, db, config);         // Load the controller routes
-loadFolder(config.paths.errorsFolder, app, db, config);              // Load error handlers, this should be the last routes loaded.
+  app.set('views', config.dirname + config.paths.viewsFolder);         // Set up the root directory for our views.
+  app.set('view engine', 'jade');                                      // Set our view engin as JADE.
 
-if(!module.parent) {                                                 // If we are the main module, aka we were not required from somewhere else.
-  app.listen(port);                                                  // Start our server listening on previously declared port.
-  if(config.mongodb.enabled)
-    console.log("Listening on port %d in %s mode with database %s.", port, app.settings.env, config.mongodb.database);
-  else
-    console.log("Listening on port %d in %s mode.", port, app.settings.env);
-}
+  // Load our routes here, in the order they will be called.
+  loadFolder(config.paths.middlewareFolder, app, db, config);          // Load the middleware controllers.
+  app.use(allowCors);                                                  // Allow cross origin requests
+  loadFolder(config.paths.controllersFolder, app, db, config);         // Load the controller routes
+  loadFolder(config.paths.errorsFolder, app, db, config);              // Load error handlers, this should be the last routes loaded.
+
+  if( ! module.parent) {                                               // If we are the main module, aka we were not required from somewhere else.
+    app.listen(port);                                                  // Start our server listening on previously declared port.
+    if(config.mongodb.enabled)
+      console.log("Listening on port %d in %s mode with database %s.", port, app.settings.env, config.mongodb.database);
+    else
+      console.log("Listening on port %d in %s mode.", port, app.settings.env);
+  }
+});
 
 /* ---------------------------------------------------- */
 /* ------------ END SERVER CONFIGURATIONS ------------- */
@@ -83,34 +87,38 @@ function loadFolder(relativePath, app, db, config) {
  * TODO: Find a way to continue on even if the database
  *       fails to connect.
  */
-function setupDatabase(app, config){
-  if(config.mongodb.enabled) {                                       // If we are configuring a Mongo database.
+function setupDatabase(app, config, next){
+  if(config.mongodb.enabled) {                                         // If we are configuring a Mongo database.
+    
+    var mongoSessionStore = new MongoStore({                           // Setup a mongo session store and code run on a connection.
+      url: config.mongodb.uri,                                         // Store the uri to connect to the database.
+      auto_reconnect: true                                             // Enable auto reconnect if the database connection is lost.
+    }, function() {                                                    // This function is called after a successful connection is setup by mongo-connect.
+      mongoose.connect(config.mongodb.uri);                            // Finally, connect to the MongoDB Database.
+      mongoose.connection.on('open', function() {                      // Once the connection is opened.
+        console.log("[ OK ] Connected to the database.");
 
-    mongoose.connect(config.mongodb.uri);                            // Connect to the database.
+        loadFolder(config.paths.modelsFolder, app, mongoose, config);  // Pull in the MongoDB schemas.
 
-    var mongoSessionStore = new MongoStore({                         // Setup a mongo session store.
-      url: config.mongodb.uri,
-      auto_reconnect: true
+        return next(undefined, mongoose);                              // Return our connection object (in this case it is just mongoose).
+      });
     });
 
-    app.use(
-      express.session({                                             // Enable express sessions.
-        secret: config.express.sessionKey,                          // Setup the secret session key.
-        store: mongoSessionStore                                    // Setup the MongoDB connection.
+    app.use(                                                           // Finally, execute our code to configure our connection to the mongodb database.
+      express.session({                                                // Enable express sessions.
+        secret: config.express.sessionKey,                             // Setup the secret session key.
+        store: mongoSessionStore                                       // Setup & connect to the MongoDB database.
       })
     );
-
-    loadFolder(config.paths.modelsFolder, app, mongoose, config);   // Pull in the MongoDB schemas.
-
-    return mongoose;                                                // Return our connected database object.
-  }
-  
-  if(config.postgresql.enabled) {                                   // If we are configuring a postgresql database.
+  } else if(config.postgresql.enabled) {                               // If we are configuring a postgresql database.
+    
     // TODO: Add configuration for postgresql.
-    return undefined;
-  }
+    console.log("[ ERROR ] Could not connect to postgresql, one was not configured.");
+    return next(undefined, undefined);
+  } else {
 
-  return undefined;                                                 // If we reached here, there was not a database to be configured.  Something is probably wrong in the config file.
+    console.log("[ ERROR ] Could not configure and connect to a database because there were not any enabled.")
+  }
 }
 
 /* Allow cross site calls.  This is required for ajax calls.
